9a1bc90b8d30f739c889cdc63a120610b1fa002f
Park Jong in <whddls9632@naver.com>
2017-11-04 18:01:22 +0900

    Path resolution

--- src/commands.c
+++ src/commands.c
@@ -54,26 +54,30 @@ int evaluate_command(int n_commands, struct single_command (*commands)[512])
 		} else if (strcmp(com->argv[0], "exit") == 0) {
 			return 1;
 			//This part takes charge of the creation of new process.
-		} else if ( com->argc == 1 ) {
-			int status;
-			int pid = fork();
-			if ( pid > 0 ) wait(&status);
-			else if ( pid == 0  ) execv(com->argv[0],com->argv);
-			else {  
-				//If Fork error is occured, this function returns -1.
+		} else if ( strstr(com->argv[0] ,"vim") || strstr(com->argv[0],"ls") || strstr(com->argv[0],"cat") ) {
+				int status;
+				int pid = fork();
+				if ( pid > 0 ) wait(&status);
+				else if ( pid == 0  ) { 
+				if( strstr(com->argv[0],"vim") ) strncpy( com->argv[0],"/usr/bin/vim\0", 13  );
+				else if( strstr(com->argv[0],"ls") ) strncpy( com->argv[0],"/bin/ls\0", 8  );
+				else if( strstr(com->argv[0],"cat") ) strncpy( com->argv[0],"/bin/cat\0", 9  );
+				execv(com->argv[0],com->argv);
+				}
+				else {  
 				perror("Fork error");
 				return -1;
-			}
-			return 0;
-		}
-		else {
-			fprintf(stderr, "%s: command not found\n", com->argv[0]);
-			return -1;
-		}
-	}
+				}
+				return 0;
+				}
+				else {
+				fprintf(stderr, "%s: command not found\n", com->argv[0]);
+				return -1;
+				}
+				}
 
-	return 0;
-}
+				return 0;
+				}
 
 void free_commands(int n_commands, struct single_command (*commands)[512])
 {
