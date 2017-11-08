599a1ebfe67fab3710d46bce9b7260c5a595aab3
Park Jong in <whddls9632@naver.com>
2017-11-08 14:41:52 +0900

    path resolution completion

--- src/commands.c
+++ src/commands.c
@@ -3,8 +3,11 @@
 #include <string.h>
 #include <assert.h>
 #include <unistd.h>
+#include <sys/io.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/un.h>
+#include <sys/socket.h>
 
 #include "commands.h"
 #include "built_in.h"
@@ -32,13 +35,16 @@ static int is_built_in_command(const char* command_name)
  * Description: Currently this function only handles single built_in commands. You should modify this structure to launch process and offer pipeline functionality.
  */
 int evaluate_command(int n_commands, struct single_command (*commands)[512])
-{
+{	
+	char path_resol[5][512] = { "/usr/local/bin/", "/usr/bin/", "/bin/", "/usr/sbin/", "/sbin/"  };
 	if (n_commands > 0) {
 		struct single_command* com = (*commands);
 
 		//If parsing result is 0, it terminates the program.
 		assert(com->argc != 0);
-
+		for( int i = 0; i < 5; i++ ) { 
+		strcat(path_resol[i],com->argv[0]);
+		}
 		int built_in_pos = is_built_in_command(com->argv[0]);
 		if (built_in_pos != -1) {
 			if (built_in_commands[built_in_pos].command_validate(com->argc, com->argv)) {
@@ -53,29 +59,92 @@ int evaluate_command(int n_commands, struct single_command (*commands)[512])
 			return 0;
 		} else if (strcmp(com->argv[0], "exit") == 0) {
 			return 1;
+		}
 			//This part takes charge of the creation of new process.
-		} else if ( strstr(com->argv[0] ,"vim") || strstr(com->argv[0],"ls") || strstr(com->argv[0],"cat") ) {
+		else if ( ( access( com->argv[0] , F_OK ) == 0 ) ) {
 				int status;
-				int pid = fork();
+				int pid;
+				
+				pid = fork();
 				if ( pid > 0 ) wait(&status);
-				else if ( pid == 0  ) { 
-				if( strstr(com->argv[0],"vim") ) strncpy( com->argv[0],"/usr/bin/vim\0", 13  );
-				else if( strstr(com->argv[0],"ls") ) strncpy( com->argv[0],"/bin/ls\0", 8  );
-				else if( strstr(com->argv[0],"cat") ) strncpy( com->argv[0],"/bin/cat\0", 9  );
-				execv(com->argv[0],com->argv);
-				}
-				else {  
+				else if ( pid == 0 ) execv(com->argv[0],com->argv);
+				else {
 				perror("Fork error");
 				return -1;
-				}
+				}	
 				return 0;
-				}
+		}
+		else if ( access( path_resol[0] , F_OK ) == 0 ) {
+				int status;
+				int pid;
+				
+				pid = fork();
+				if ( pid > 0 ) wait(&status);
+				else if ( pid == 0 ) execv(path_resol[0],com->argv);
 				else {
+				perror("Fork error");
+				return -1;
+				}	
+				return 0;
+		}
+		else if ( access( path_resol[1], F_OK ) == 0  ) {
+				int status;
+				int pid;
+				
+				pid = fork();
+				if ( pid > 0 ) wait(&status);
+				else if ( pid == 0 ) execv(path_resol[1],com->argv);
+				else {
+				perror("Fork error");
+				return -1;
+				}	
+				return 0;
+		}
+		else if ( access( path_resol[2], F_OK ) == 0  ) {
+				int status;
+				int pid;
+				
+				pid = fork();
+				if ( pid > 0 ) wait(&status);
+				else if ( pid == 0 ) execv(path_resol[2],com->argv);
+				else {
+				perror("Fork error");
+				return -1;
+				}	
+				return 0;
+		}
+		else if ( access( path_resol[3], F_OK ) == 0  ) {
+				int status;
+				int pid;
+				
+				pid = fork();
+				if ( pid > 0 ) wait(&status);
+				else if ( pid == 0 ) execv(path_resol[3],com->argv);
+				else {
+				perror("Fork error");
+				return -1;
+				}	
+				return 0;
+		}
+		else if ( access( path_resol[4], F_OK ) == 0  ) {
+				int status;
+				int pid;
+				
+				pid = fork();
+				if ( pid > 0 ) wait(&status);
+				else if ( pid == 0 ) execv(path_resol[4],com->argv);
+				else {
+				perror("Fork error");
+				return -1;
+				}	
+				return 0;
+		}
+		else {
 				fprintf(stderr, "%s: command not found\n", com->argv[0]);
 				return -1;
 				}
 				}
-
+				
 				return 0;
 				}
 
