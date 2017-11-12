0fc63435cf3dace5a9bedb49336e0808bcc3b353
Park Jong in <whddls9632@naver.com>
2017-11-12 15:13:40 +0900

    IPC Almost done

--- include/commands.h
+++ include/commands.h
@@ -11,4 +11,8 @@ int evaluate_command(int n_commands, struct single_command (*commands)[512]);
 
 void free_commands(int n_commands, struct single_command (*commands)[512]);
 
+void *pipe_server(struct single_command (*com));
+void pipe_commands(int n_commands, struct single_command (*com));
+int isBack( struct single_command (*com), int n_commands );
+
 #endif // MYSH_COMMANDS_H_
--- src/commands.c
+++ src/commands.c
@@ -9,10 +9,15 @@
 #include <sys/stat.h>
 #include <sys/un.h>
 #include <sys/socket.h>
+#include <arpa/inet.h>
 #include <pthread.h>
+#include <fcntl.h>
 
 #include "commands.h"
 #include "built_in.h"
+
+#define SOCK_PATH "/tmp/test_server.dat"
+#define BUFF_SIZE 1024
 
 static struct built_in_command built_in_commands[] = {
 	{ "cd", do_cd, validate_cd_argv },
@@ -40,12 +45,13 @@ static int is_built_in_command(const char* command_name)
  */
 int evaluate_command(int n_commands, struct single_command (*commands)[512])
 {	
-	int pid, status, num = 1;
+	int pid, status, num = 1, back = 0;
 	char short_path[5][512] = { "" };
 	struct stat buf[6];
+	struct single_command* com = (*commands);
+	back = isBack(com, n_commands);
 	for( int i = 0; i < 5; i++ ) strcpy(short_path[i], path_resol[i]);
 	if (n_commands == 1) {
-		struct single_command* com = (*commands);
 		//If parsing result is 0, it terminates the program.
 		assert(com->argc != 0);
 		stat(com->argv[0], &buf[0]);
@@ -54,6 +60,7 @@ int evaluate_command(int n_commands, struct single_command (*commands)[512])
 		strcat(short_path[i],com->argv[0]);
 		stat(short_path[i],&buf[i+1]);
 		}
+	
 		int built_in_pos = is_built_in_command(com->argv[0]); 
 		if (built_in_pos != -1) {
 			if (built_in_commands[built_in_pos].command_validate(com->argc, com->argv)) {
@@ -130,7 +137,7 @@ int evaluate_command(int n_commands, struct single_command (*commands)[512])
 				}
 		
 				}
-	else if( n_commands > 1 );
+	else if( n_commands > 1 ) pipe_commands(n_commands, com);
 				
 				return 0;
 				}
@@ -150,4 +157,94 @@ void free_commands(int n_commands, struct single_command (*commands)[512])
 	}
 
 	memset((*commands), 0, sizeof(struct single_command) * n_commands);
+}
+void pipe_commands( int n_commands, struct single_command (*com) ) {
+	int pid, pid2, status;
+	pthread_t th;
+	pthread_create(&th, NULL, pipe_server, com);
+	
+	pid = fork();
+	if( pid == 0 ) {
+		//synchronization problem...
+		sleep(1);
+		int client_socket;
+		struct sockaddr_un server_sockaddr;
+		char buffer[BUFF_SIZE+5];
+		
+		client_socket = socket(PF_FILE, SOCK_STREAM, 0);
+		memset(&server_sockaddr, 0, sizeof(server_sockaddr));
+		server_sockaddr.sun_family = AF_UNIX;
+		strcpy(server_sockaddr.sun_path, SOCK_PATH);
+	  	if( connect(client_socket, (struct sockaddr*)&server_sockaddr, sizeof(server_sockaddr)) == -1 ) {
+			printf("Connection failed!!\n");
+			exit(1);
+		}
+		pid2 = fork();
+		if(pid2 == 0) {
+			//Close the file descriptor stdin, stdout
+			close(STDIN_FILENO);
+			close(STDOUT_FILENO);
+			if ( dup2(client_socket, STDOUT_FILENO) == -1  ) {
+				printf("Connect Stdin to stdout is failed!!\n");
+				exit(1);
+			}		
+			evaluate_command(1,com);
+			exit(0);
+		}
+		wait(&status);
+		close(client_socket);
+		exit(0);
+	}
+	else wait(&status);
+	pthread_join(th,  NULL);
+}
+void *pipe_server(struct single_command (*com)) {
+	struct single_command data[512] = {};
+	int server_socket, client_socket, client_size, status, rc;
+	struct sockaddr_un server_sockaddr;
+	struct sockaddr_un client_sockaddr;
+	char buff_server[BUFF_SIZE+5], buff_client[BUFF_SIZE+5];
+	
+	memcpy(data,com+1,sizeof(struct single_command)*511);
+
+	if( access(SOCK_PATH, F_OK ) == 0  ) unlink(SOCK_PATH);
+	server_socket = socket(PF_FILE, SOCK_STREAM, 0);
+	memset(&server_sockaddr, 0, sizeof(server_sockaddr));
+	server_sockaddr.sun_family = AF_UNIX;
+	strcpy(server_sockaddr.sun_path, SOCK_PATH);
+		
+	rc = bind(server_socket, (struct sockaddr*)&server_sockaddr,sizeof(server_sockaddr));
+	if( rc == -1 ) {
+		printf("Bind error occured!!\n");
+		close(server_socket);
+		exit(1);
+	}
+	while(1) {
+		if ( listen(server_socket,5) == -1  ) {
+			printf("Listen error occured!!\n");
+			exit(1);
+		}
+		client_socket = accept(server_socket, (struct sockaddr*)&client_sockaddr, &client_size);
+		assert(client_socket != -1);
+		if( fork() == 0 ) {
+			close(STDIN_FILENO);
+			dup2(client_socket, STDIN_FILENO);
+			evaluate_command(1, data);
+			exit(0);  
+		}
+		wait(&status);
+		close(client_socket);
+		pthread_exit(0);
+	}
+}
+//If the command contains "&" character, it retuns 1. If not, 0.
+int isBack( struct single_command (*com), int n_commands  ) {
+	int result = 0;
+	for( int i = 0; i < n_commands; i++ ) {
+		for( int j = 0; j < com[i].argc; j++  ) {
+			if( strcmp(com[i].argv[j],"&") == 0 ) result++;
+		}
+		if( result > 0 ) break;
+	}
+	return result;
 }
